896ef175cb5dcd8754051d42e2e077895b93137d
Angela <angelapalmasanchez@gmail.com>
2021-07-01 20:01:49 +0200

    bbdd actualizada anyadida provincia

--- BBDDproyectoFinal1.sql
+++ BBDDproyectoFinal1.sql
@@ -56,7 +56,7 @@ CREATE TABLE `t_provincias` (
   `nombre_provincia` varchar(80) NOT NULL,
   `nombre_comunidad` varchar(80) NOT NULL,
   PRIMARY KEY (`id_prov`)
-) ENGINE=InnoDB AUTO_INCREMENT=55 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=56 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -65,7 +65,7 @@ CREATE TABLE `t_provincias` (
 
 LOCK TABLES `t_provincias` WRITE;
 /*!40000 ALTER TABLE `t_provincias` DISABLE KEYS */;
-INSERT INTO `t_provincias` VALUES (1,'Alicante','C. Valenciana'),(2,'Valencia','C. Valenciana'),(3,'Castellon','C. Valenciana'),(4,'Murcia','Murcia'),(5,'Almeria','Andalucia'),(6,'Granada','Andalucia'),(7,'Sevilla','Andalucia'),(8,'Malaga','Andalucia'),(9,'Jaen','Andalucia'),(10,'Cadiz','Andalucia'),(11,'Huelva','Andalucia'),(12,'Cordoba','Andalucia'),(13,'Badajoz','Extremadura'),(14,'Caceres','Extremadura'),(15,'Salamanca','Castilla y Leon'),(16,'Avila','Castilla y Leon'),(17,'Segovia','Castilla y Leon'),(18,'Valladolid','Castilla y Leon'),(19,'Soria','Castilla y Leon'),(20,'Burgos','Castilla y Leon'),(21,'Palencia','Castilla y Leon'),(22,'Zamora','Castilla y Leon'),(23,'Leon','Castilla y Leon'),(24,'Pontevedra','Galicia'),(25,'A Coruña','Galicia'),(26,'Ourense','Galicia'),(27,'Lugo','Galicia'),(28,'Asturias','Asturias'),(29,'Cantabria','Cantabria'),(30,'Vizcaya ','Pais Vasco'),(31,'Alava','Pais Vasco'),(32,'Guipuzcoa','Pais Vasco'),(33,'Navarra','Navarra'),(34,'La Rioja','La Rioja'),(35,'Huesca ','Aragon'),(36,'Zaragoza','Aragon'),(37,'Teruel','Aragon'),(38,'LLeida','Catalunya'),(39,'Tarragona','Catalunya'),(40,'Barcelona','Catalunya'),(41,'Girona','Catalunya'),(42,'Guadalajara','Castilla La Mancha'),(43,'Cuenca','Castilla La Mancha'),(44,'Albacete','Castilla La Mancha'),(45,'Ciudad Real','Castilla La Mancha'),(46,'Toledo','Castilla La Mancha'),(47,'Madrid','Madrid'),(48,'Menorca','Islas Baleares'),(49,'Mallorca ','Islas Baleares'),(50,'Ibiza','Islas Baleares'),(51,'Ceuta','Ceuta '),(52,'Melilla','Melilla'),(53,'Gran Canaria','Islas Canarias'),(54,'Tenerife','Islas Canarias');
+INSERT INTO `t_provincias` VALUES (1,'Alicante','C. Valenciana'),(2,'Valencia','C. Valenciana'),(3,'Castellon','C. Valenciana'),(4,'Murcia','Murcia'),(5,'Almeria','Andalucia'),(6,'Granada','Andalucia'),(7,'Sevilla','Andalucia'),(8,'Malaga','Andalucia'),(9,'Jaen','Andalucia'),(10,'Cadiz','Andalucia'),(11,'Huelva','Andalucia'),(12,'Cordoba','Andalucia'),(13,'Badajoz','Extremadura'),(14,'Caceres','Extremadura'),(15,'Salamanca','Castilla y Leon'),(16,'Avila','Castilla y Leon'),(17,'Segovia','Castilla y Leon'),(18,'Valladolid','Castilla y Leon'),(19,'Soria','Castilla y Leon'),(20,'Burgos','Castilla y Leon'),(21,'Palencia','Castilla y Leon'),(22,'Zamora','Castilla y Leon'),(23,'Leon','Castilla y Leon'),(24,'Pontevedra','Galicia'),(25,'A Coruña','Galicia'),(26,'Ourense','Galicia'),(27,'Lugo','Galicia'),(28,'Asturias','Asturias'),(29,'Cantabria','Cantabria'),(30,'Vizcaya ','Pais Vasco'),(31,'Alava','Pais Vasco'),(32,'Guipuzcoa','Pais Vasco'),(33,'Navarra','Navarra'),(34,'La Rioja','La Rioja'),(35,'Huesca ','Aragon'),(36,'Zaragoza','Aragon'),(37,'Teruel','Aragon'),(38,'Lerida','Catalunya'),(39,'Tarragona','Catalunya'),(40,'Barcelona','Catalunya'),(41,'Girona','Catalunya'),(42,'Guadalajara','Castilla La Mancha'),(43,'Cuenca','Castilla La Mancha'),(44,'Albacete','Castilla La Mancha'),(45,'Ciudad Real','Castilla La Mancha'),(46,'Toledo','Castilla La Mancha'),(47,'Madrid','Madrid'),(48,'Menorca','Islas Baleares'),(49,'Mallorca ','Islas Baleares'),(50,'Ibiza','Islas Baleares'),(51,'Ceuta','Ceuta '),(52,'Melilla','Melilla'),(53,'Gran Canaria','Islas Canarias'),(54,'Tenerife','Islas Canarias'),(55,'Huesca','Aragon');
 /*!40000 ALTER TABLE `t_provincias` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -134,4 +134,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-07-01 16:04:22
+-- Dump completed on 2021-07-01 20:01:23
